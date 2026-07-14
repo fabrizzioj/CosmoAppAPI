5bc4d8d4cbe92383796ddbdba879b495f52d7548
fabrizzioj <fabrizzio.jiglau@softwire.com>
2026-07-14 14:17:18 +0300

    added apod endpoint

---
+++ Controllers/AstronomyPictureController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CosmoAppAPI.Services;
+
+namespace CosmoAppAPI.Controllers;
+
+[ApiController]
+[Route("apod/today")]
+public class AstronomyPictureController : ControllerBase
+{
+    private readonly AstronomyPictureService _pictureService;
+    
+    public AstronomyPictureController(AstronomyPictureService pictureService)
+    {
+        _pictureService = pictureService;
+    }
+    
+    [HttpGet(Name = "GetAstronomyPicture")]
+    public async Task<ActionResult<AstronomyPicture>> Get()
+    {
+        try
+        {
+            var picture = await _pictureService.GetTodayPictureAsync();
+            if (picture == null) 
+                return NotFound();
+            return Ok(picture); 
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "An error occurred while fetching the astronomy picture.");
+        }
+    }
+}
